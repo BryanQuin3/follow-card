a689f07c81f1c24649855a87dedbae346e94edd6
Bryan Quintana <bryandise@gmail.com>
2023-07-30 17:46:18 -0400

    first class

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : 'https://BryanQuin3.github.io/follow-card',
 })
